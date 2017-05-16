9a1d27aada2fa04100bb0a986a550caf65205019
Michał Dzióbek <michal.dziobek@outlook.com>
2017-05-16 22:05:56 +0200

    DO NOT USE THIS SOLUTION FOR FURTHER DEVELOPEMENT. Only for SleuthKit testing purposes.

---
+++ Tester/Program.cs
@@ -0,0 +1,124 @@
+﻿using SleuthKit;
+using SleuthKit.Structs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Directory = SleuthKit.Directory;
+using File = SleuthKit.File;
+
+namespace Tester
+{
+    class Program
+    {
+        private static readonly IList<string> FilePaths = new List<string>();
+        private static int allFileCount;
+
+        /// <summary>
+        ///     The file count.
+        /// </summary>
+        private static int jpgFileCount;
+        static void Main(string[] args)
+        {
+            var file = new FileInfo("..//..//resources/USB-disk-image-FAT.E01");
+            var diskImage = new DiskImage(file);
+            int volumeAdress = 2;
+
+            using (VolumeSystem volumeSystem = diskImage.OpenVolumeSystem())
+            {
+                Volume volume = volumeSystem.Volumes.SingleOrDefault(v => v.Address == volumeAdress);
+
+                //Assert.NotNull(volume);
+
+                using (FileSystem fileSystem = volume.OpenFileSystem())
+                {
+                    //count += CountFiles(fileSystem.OpenRootDirectory());
+                    fileSystem.WalkDirectories(
+                        FindFiles_DirectoryWalkCallback,
+                        DirWalkFlags.Recurse | DirWalkFlags.Unallocated);
+                }
+                Console.ReadKey();
+                //Assert.AreEqual(37, FilePaths.Count()); //I think it should be 63 || Bala: Autopsy shows me that there are only 30 files
+            }
+
+
+        }
+
+        private static WalkReturnEnum FileCount_DirectoryWalkCallback(
+            ref TSK_FS_FILE file,
+            string directoryPath,
+            IntPtr dataPtr)
+        {
+            if (file.Name.ToString().Contains("jpg"))
+            {
+                jpgFileCount++;
+            }
+            allFileCount++;
+            return WalkReturnEnum.Continue;
+        }
+
+        /// <summary>
+        ///     Callback function that is called for each file name during directory walk. (FileSystem.WalkDirectories)
+        /// </summary>
+        /// <param name="file">
+        ///     The file struct.
+        /// </param>
+        /// <param name="directoryPath">
+        ///     The directory path.
+        /// </param>
+        /// <param name="dataPtr">
+        ///     Pointer to data that is passed to the callback function each time.
+        /// </param>
+        /// <returns>
+        ///     Value to control the directory walk.
+        /// </returns>
+        private static WalkReturnEnum FindFiles_DirectoryWalkCallback(
+            ref TSK_FS_FILE file,
+            string directoryPath,
+            IntPtr dataPtr)
+        {
+            FilePaths.Add(string.Format("{0}{1}", directoryPath, file.Name));
+            return WalkReturnEnum.Continue;
+        }
+
+        private int CountFiles(SleuthKit.Directory directory)
+        {
+            int count = 0;
+
+            if (directory == null)
+            {
+                return 0;
+            }
+
+            foreach (Directory subDirectory in directory.Directories)
+            {
+                count += this.CountFiles(subDirectory);
+            }
+
+            count += directory.Files.Count();
+
+            return count;
+        }
+
+        private IEnumerable<String> FindFiles(Directory directory)
+        {
+            if (directory != null)
+            {
+                foreach (Directory subDirectory in directory.Directories)
+                {
+                    foreach (String path in this.FindFiles(subDirectory))
+                    {
+                        yield return path;
+                    }
+                }
+
+                foreach (File file in directory.Files)
+                {
+                    yield return file.Path;
+                }
+            }
+        }
+    }
+}
---
+++ src/Structs/FATFS_INFO.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SleuthKit.Structs
+{
+    [StructLayout(LayoutKind.Explicit,
+#if Bit32
+        Size = 17424
+#elif Bit64
+        Size = 17616
+#endif
+    )]
+    public struct FATFS_INFO
+    {
+        [FieldOffset(0)]
+        private TSK_FS_INFO fs_info;
+
+        /// <summary>
+        /// super block
+        /// </summary>
+#if Bit32
+        [FieldOffset(16860)]
+#elif Bit64
+        [FieldOffset(17024)]
+#endif
+        //private IntPtr sb_ptr;
+        [MarshalAsAttribute(UnmanagedType.ByValArray, SizeConst = 512)]
+        private byte[] boot_sector_buffer;
+
+        internal TSK_FS_INFO tsk_fs_info
+        {
+            get
+            {
+                return fs_info;
+            }
+        }
+
+        internal FATXXFS_SB sb
+        {
+            get
+            {
+                GCHandle handle = GCHandle.Alloc(boot_sector_buffer, GCHandleType.Pinned);
+                FATXXFS_SB superblock = (FATXXFS_SB)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(FATXXFS_SB));
+                handle.Free();
+                return superblock;
+            }
+        }
+    }
+}
