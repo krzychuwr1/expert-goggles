114447a585f00cca080d88a3533a80dcf4462db1
Krzysztof Wrona <krzychuwr1@gmail.com>
2017-11-20 17:55:05 +0100

    sleuthkit sharp nuspec

--- LibraryPrototype/sleuthkit-sharp/Properties/AssemblyInfo.cs
+++ LibraryPrototype/sleuthkit-sharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("sleuthkit-sharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("test")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ZTC")]
 [assembly: AssemblyProduct("sleuthkit-sharp")]
